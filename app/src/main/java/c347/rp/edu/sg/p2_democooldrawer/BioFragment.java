97fdd71dc7a22275aa019a00b720cf2ade610e45
14056921 <14056921@myrp.edu.sg>
2017-08-11 16:48:57 +0800

    P02 Demo CoolDrawer

---
+++ app/src/main/java/c347/rp/edu/sg/p2_democooldrawer/BioFragment.java
@@ -0,0 +1,32 @@
+package c347.rp.edu.sg.p2_democooldrawer;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BioFragment extends Fragment {
+
+    View view;
+
+
+    public BioFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_bio, container, false);
+        return view;
+    }
+
+}
